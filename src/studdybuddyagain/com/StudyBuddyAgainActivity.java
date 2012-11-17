8cde7761bfb7c00562483ed77e4186da85846fe6
Ben Tiggelaar <bentigg@gmail.com>
2012-11-16 21:51:32 -0600

    first commit

---
+++ src/studdybuddyagain/com/StudyBuddyAgainActivity.java
@@ -0,0 +1,13 @@
+package studdybuddyagain.com;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StudyBuddyAgainActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
